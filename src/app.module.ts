c807543870945e5bfd0004ef683f91307fedd543
wjdtmddnr24 <wjdtmddnr24@naver.com>
2021-04-23 16:51:53 +0000

    Implement TypeORM PostgreSQL Connecting

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,23 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { WebhooksModule } from './webhooks/webhooks.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, WebhooksModule, NotificationsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      synchronize: true,
+    }),
+    UsersModule,
+    WebhooksModule,
+    NotificationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
