6a4e425e4732f84c2a70424ca49b1f21ccaf7e05
wjdtmddnr24 <wjdtmddnr24@naver.com>
2021-05-03 19:07:46 +0000

    Update Import Entities

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -8,6 +8,7 @@ import { TelegramService } from './dispatchers/telegram.service';
 import { NotificationsController } from './notifications.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationHistory } from './notification-history.entity';
+import { Notification } from './notification.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Notification, NotificationHistory])],
--- src/webhooks/webhooks.service.ts
+++ src/webhooks/webhooks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Webhook } from './webhook.entity';
+import { Webhook } from './entities/webhook.entity';
 
 @Injectable()
 export class WebhooksService {
